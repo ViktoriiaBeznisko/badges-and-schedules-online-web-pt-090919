ff9bf734dcd1cb0836ee14cbc4d30a45e356928f
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-20 12:36:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,21 @@ def batch_badge_creator(attendees)
   return badges_list
 end
 
+def 
+
+describe '#assign_rooms' do
+
+    # Question 3
+
+    it 'should return a list of welcome messages and room assignments' do
+      expect(assign_rooms(attendees)).to eq(room_assignments)
+    end
+    it 'should not hard-code the response' do
+      expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
+    end
+
+  end
+
 
   #attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   #attendees.each do |name|
