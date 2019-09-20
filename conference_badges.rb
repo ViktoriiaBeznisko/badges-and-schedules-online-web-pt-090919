344a37576d7bddcbd0a29d4852c758f1f2bf752a
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-20 12:47:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,17 +21,9 @@ def assign_rooms(attendees)
   return list_of_welcome_messages
 end
 
-    it 'should return a list of welcome messages and room assignments' do
-      expect(assign_rooms(attendees)).to eq(room_assignments)
-    end
-    it 'should not hard-code the response' do
-      expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
-    end
+    #it 'should return a list of welcome messages and room assignments' do
+      #expect(assign_rooms(attendees)).to eq(room_assignments)
 
-  end
-
-
-  #attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  #attendees.each do |name|
- #print "Hello, my name is #{name}."
-  #end
+    #it 'should not hard-code the response' do
+     # expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
+   
