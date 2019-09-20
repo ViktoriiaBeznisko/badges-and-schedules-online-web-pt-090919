309ebcdf06252797a5cc1a2d1038125deb55b5c9
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-20 12:21:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,16 @@
 # Write your code here.
 def badge_maker(name)
-return "Hello, my name is #{name}."
+bages = "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  badges_list = []
+  attendees.each do |name|
+    badges_list.push("Hello, my name is #{name}.")
+  end
+  return badges_list
+end
+
 
   #attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   #attendees.each do |name|
