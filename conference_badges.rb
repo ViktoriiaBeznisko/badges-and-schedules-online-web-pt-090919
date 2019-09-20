5fde80d2711c47ee7695fe83d417ee9b4facb575
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-20 12:21:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+  #attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  #attendees.each do |name|
+ #print "Hello, my name is #{name}."
+  #end
